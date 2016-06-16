94eaad23e1736ef9d67814e6124e9a26cc964b72
Atul Sikaria <atulmsft@outlook.com>
2016-06-15 20:03:09 -0700

    Added Util method implementations

--- src/main/java/com/microsoft/azure/datalake/store/Utils.java
+++ src/main/java/com/microsoft/azure/datalake/store/Utils.java
@@ -1,6 +1,12 @@
 package com.microsoft.azure.datalake.store;
 
-import java.io.InputStream;
+import com.microsoft.azure.datalake.store.protocol.Core;
+import com.microsoft.azure.datalake.store.protocol.OperationResponse;
+import com.microsoft.azure.datalake.store.protocol.RequestOptions;
+import com.microsoft.azure.datalake.store.retrypolicies.ExponentialOnThrottlePolicy;
+import com.microsoft.azure.datalake.store.retrypolicies.NoRetryPolicy;
+
+import java.io.*;
 import java.net.URI;
 
 /**
@@ -20,50 +26,98 @@ public class Utils {
         this.client = client;
     }
 
-    public boolean checkDirectoryExists(String directoryName) {
+    public boolean checkDirectoryExists(String directoryName) throws ADLException {
+        if (directoryName == null || directoryName.trim().equals(""))
+            throw new IllegalArgumentException("directory name cannot be null");
+
+        ADLDirectoryInfo di = client.getDirectoryInfo(directoryName);
+        try {
+            di.getDirectoryEntry();
+        } catch (ADLException ex) {
+            if (ex.httpResponseCode == 404) return false;
+            else throw ex;
+        }
         return true;
     }
 
-    public boolean checkFileExists(String fileName) {
+    public boolean checkFileExists(String filename) throws ADLException {
+        if (filename == null || filename.trim().equals(""))
+            throw new IllegalArgumentException("filename cannot be null");
+
+        ADLFileInfo fi = client.getFileInfo(filename);
+        try {
+            fi.getDirectoryEntry();
+        } catch (ADLException ex) {
+            if (ex.httpResponseCode == 404) return false;
+            else throw ex;
+        }
         return true;
     }
 
-    public boolean createDirectory(String directoryName) {
-        return true;
+    public void createDirectory(String directoryName) throws ADLException {
+        if (directoryName == null || directoryName.trim().equals(""))
+            throw new IllegalArgumentException("directory name cannot be null");
+
+        ADLDirectoryInfo di = client.getDirectoryInfo(directoryName);
+        di.create();
     }
 
-    public boolean createEmptyFile(boolean overwriteIfExists) {
-        return true;
+    public void createEmptyFile(String filename, boolean overwriteIfExists) throws IOException {
+        if (filename == null || filename.trim().equals(""))
+            throw new IllegalArgumentException("filename cannot be null");
+
+        ADLFileInfo fi = client.getFileInfo(filename);
+        OutputStream out = fi.createFromStream(overwriteIfExists);
+        out.close();
     }
 
-    public boolean upload(String fileName, String localFileName, boolean overwriteIfExists)  {
-        return true;
+    public void upload(String filename, String localFilename, boolean overwriteIfExists) throws IOException  {
+        if (localFilename == null || localFilename.trim().equals(""))
+            throw new IllegalArgumentException("localFilename cannot be null");
+
+        FileInputStream in = new FileInputStream(localFilename);
+        upload(filename, in, overwriteIfExists);
     }
 
-    public boolean upload(String fileName, InputStream stream, boolean overwriteIfExists)  {
-        return true;
+    public void upload(String filename, InputStream in, boolean overwriteIfExists) throws IOException {
+        if (filename == null || filename.trim().equals(""))
+            throw new IllegalArgumentException("filename cannot be null");
+        if (in == null) throw new IllegalArgumentException("InputStream cannot be null");
+
+        ADLFileInfo fi = client.getFileInfo(filename);
+        OutputStream out = fi.createFromStream(overwriteIfExists);
+        byte[] buffer = new byte[4 * 1000 * 1000];
+        int n;
+
+        while ((n=in.read(buffer)) != -1) {
+            out.write(buffer, 0, n);
+        }
+        out.close();
+        in.close();
     }
 
-    public boolean uploadFromUrl(String fileName, URI sourceUri, boolean overwriteIfExists) {
-        return true;
-    }
 
     /**
      * Does an atomic append to the file - the append either succeeds fully, or fails
      * fully - the contents are not partially appended.
      *
-     * @param fileName name of file to append to
+     * @param filename name of file to append to
      * @param bytesToAppend
      *          the byte buffer to append. Max buffer size can be 4MB.
      * @return returns true on success
      */
-    public boolean appendBytes(String fileName, byte[] bytesToAppend) {
-        return true;
+    public void appendBytes(String filename, byte[] bytesToAppend) throws IllegalArgumentException, ADLException {
+        if (filename == null || filename.trim().equals(""))
+            throw new IllegalArgumentException("filename cannot be null");
+        if (bytesToAppend == null || bytesToAppend.length == 0)  return; // nothing to append
+        if (bytesToAppend.length > 4*1024*1024) throw new IllegalArgumentException("maximum of 4MB can be appended in one request");
+
+        RequestOptions opts = new RequestOptions();
+        opts.retryPolicy = new NoRetryPolicy();
+        OperationResponse resp = new OperationResponse();
+        Core.concurrentAppend(filename, bytesToAppend, 0, bytesToAppend.length, true, client, opts, resp);
+        if (!resp.successful) {
+            throw Core.getExceptionFromResp(resp, "Error appending to file " + filename);
+        }
     }
-
-    public boolean appendBytes(String fileName, byte[] bytesToAppend, long fileOffset) {
-        return true;
-    }
-
-
 }
